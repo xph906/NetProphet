b464a115265afa87b0075e4b8f0f7d0bd9b504a6
xph906 <xiangpan2011@u.northwestern.edu>
2016-01-25 08:28:53 -0600

    added one file

---
+++ okhttp/src/main/java/netprophet/PostCallInfoTask.java
@@ -0,0 +1,52 @@
+package netprophet;
+import static okhttp3.internal.Internal.logger;
+
+import java.io.IOException;
+import java.util.logging.Level;
+
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class PostCallInfoTask implements Runnable{
+    private String serverURL;
+    private String contents;
+    private OkHttpClient client;
+    private MediaType jsonType;
+    
+	public PostCallInfoTask(String contents, String url){
+    	if(url == null){
+    		//TODO: read from configure file
+    		serverURL = "";
+    	}
+    	else {
+    		this.serverURL = url;
+    	}
+    	this.contents = contents;
+    	this.client = new OkHttpClient();
+    	this.jsonType = MediaType.parse("application/json; charset=utf-8");
+    }
+	  
+	@Override
+	public void run() {
+		//logger.log(Level.INFO, "prepares send json object: "+this.contents);
+		RequestBody body = RequestBody.create(this.jsonType, this.contents);
+		Request request = new Request.Builder()
+			.url(this.serverURL)
+			.post(body)
+			.build();
+		
+		try {
+			Response response = client.newCall(request).execute();
+			String str = response.body().string();
+			//logger.log(Level.INFO, 
+			//		String.format("postCallInfoToServerTask succeed: %s", str));
+		} catch (IOException e) {
+			//logger.log(Level.WARNING, 
+			//		String.format("postCallInfoToServerTask failed: %s", e.toString()));
+		}
+	}
+	  
+  }
